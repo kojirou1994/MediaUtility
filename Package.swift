56f88fb34bf659c5b1f6f17d89d14facb2317f93
kojirou <kojirouhtc@gmail.com>
2020-06-05 03:01:19 +0800

    update

--- Package.swift
+++ Package.swift
@@ -3,29 +3,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "MediaUtility",
-    products: [
-        .library(
-            name: "MediaUtility",
-            targets: ["MediaUtility"]),
-        .library(
-            name: "MediaTools",
-            targets: ["MediaTools"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/kojirou1994/Kwift.git", from: "0.5.0"),
-        .package(url: "git@github.com:kojirou1994/Executable.git", from: "0.0.1")
-    ],
-    targets: [
-        .target(
-            name: "MediaUtility",
-            dependencies: [.product(name: "KwiftExtension", package: "Kwift")]),
-        .target(
-            name: "MediaTools",
-            dependencies: ["Executable", "MediaUtility"]
-        ),
-        .testTarget(
-            name: "MediaUtilityTests",
-            dependencies: ["MediaUtility", "MediaTools"]),
-    ]
+  name: "MediaUtility",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  products: [
+    .library(
+      name: "MediaUtility",
+      targets: ["MediaUtility"]),
+    .library(
+      name: "MediaTools",
+      targets: ["MediaTools"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/kojirou1994/Kwift.git", .upToNextMinor(from: "0.6.0")),
+    .package(url: "git@github.com:kojirou1994/Executable.git", from: "0.0.1")
+  ],
+  targets: [
+    .target(
+      name: "MediaUtility",
+      dependencies: [.product(name: "KwiftExtension", package: "Kwift")]),
+    .target(
+      name: "MediaTools",
+      dependencies: ["Executable", "MediaUtility"]
+    ),
+    .testTarget(
+      name: "MediaUtilityTests",
+      dependencies: ["MediaUtility", "MediaTools"]),
+  ]
 )
