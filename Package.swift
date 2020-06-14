5ce7dbc5f4e10d50949b647c5e8fba2b84775c76
kojirou <kojirouhtc@gmail.com>
2020-06-15 00:54:01 +0800

    switch to forked XMLParsing

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/kojirou1994/Kwift.git", .upToNextMinor(from: "0.6.0")),
     .package(url: "git@github.com:kojirou1994/Executable.git", from: "0.0.1"),
-    .package(url: "https://github.com/ShawnMoore/XMLParsing.git", from: "0.0.3")
+    .package(url: "https://github.com/kojirou1994/XMLParsing.git", .upToNextMinor(from: "0.1.0"))
   ],
   targets: [
     .target(
