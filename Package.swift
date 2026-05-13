c42177e024a1271ba7b2143c884a59b3079a04c8
kojirou <kojirouhtc@gmail.com>
2026-05-14 03:31:35 +0800

    update

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "MediaUtility",
   platforms: [
-    .macOS(.v10_15)
+    .macOS(.v11)
   ],
   products: [
     .library(name: "MediaUtility", targets: ["MediaUtility"]),
